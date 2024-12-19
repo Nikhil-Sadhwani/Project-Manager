c3e527df199f9ecadfae03acda86a3ea39ead63f
Nikhil-Sadhwani <137155995+Nikhil-Sadhwani@users.noreply.github.com>
2024-12-19 13:01:31 +0530

    Cors updated

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,13 @@ connectDB();
 const app = express();
 
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 
 app.use("/api/projects", projectRoutes);
